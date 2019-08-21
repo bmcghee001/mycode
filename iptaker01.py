339c14de26c855852c32207b696fbd2363f41dd1
bmcghee001 <bmcghee001@gmail.com>
2019-08-21 14:00:13 +0000

    This is just a test commit message #2

---
+++ iptaker01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+## the line below creates a single string that is passed to print()
+#print("You told me the IPv4 address is : " + user_input)
+
+## print() can be given a series of objects separated by a comma
+
+user_input = input("Please enter an IPv4 address: ")
+print("You told me the IPv4 address is:", user_input)
+
