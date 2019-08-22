b3922c34a397df2fe122b6f1d7b721a102759c11
bmcghee001 <bmcghee001@gmail.com>
2019-08-22 14:26:02 +0000

    Day_4-StringMethod_Lab

---
+++ strings/stringmethod01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+print()
+print()
+
+#create a small string
+lilstring = "Alta3 Research offers classes on Puthon coding"
+newlist = lilstring.split(" ") # this returns a list
+print(newlist)
+print()
+print()
+
+#create a list of strings
+myiplist = ["192", "168", "0", "12"]
+print()
+print()
+
+#set singleip as the result of joining the list myiplist around the " "
+singleip = ".".join(myiplist)
+print()
+print()
+
+
+#display singleip
+print(singleip)
+print()
+print()
